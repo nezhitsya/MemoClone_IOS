9238ccf06e31156e4ce7e5233038179fa8be91b5
nezhitsya <huyn9295@naver.com>
2021-02-12 17:12:40 +0900

    Initial Commit

---
+++ Memo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  Memo
+//
+//  Created by 이다영 on 2021/02/09.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Lorem Ipsum")
+    ]
+}
